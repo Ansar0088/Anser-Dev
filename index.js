fa52989a406d33f133c1385b0b56bd2e458ca689
Ansar0088 <ansaransari151204@gmail.com>
2022-11-23 13:05:30 +0500

    f

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// var 
+// value store moeed ki shadi hoi  hai ke nahe;
+// alert main show the result
+
+// let moeedSb ; 
+// moeedSb = prompt("Is mooeed has got marridge!",'');
+// if(moeedSb ==="yes"){
+//     alert("majid give me money for the trip!")
+// }else{
+//     alert("majid please allow me to marrige!")
+// }
+if (time < 10) {
+    greeting = "Good morning";
+  } else if (time < 20) {
+    greeting = "Good day";
+  } else {
+    greeting = "Good evening";
+  }
